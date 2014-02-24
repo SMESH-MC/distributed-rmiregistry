ae7cfbab8484115ce8839255e2199e6edec88334
tjakobs <tudilli@web.de>
2014-02-24 10:42:14 +0100

    ClientStart angelegt

---
+++ vs1-ws2013/src/client/ClientStart.java
@@ -0,0 +1,21 @@
+/**
+ * Startklasse des Client-Programms
+ */
+package client;
+
+/**
+ * @author Thorsten
+ *
+ */
+public class ClientStart {
+
+	/**
+	 * Startpunkt fuer den Client.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
