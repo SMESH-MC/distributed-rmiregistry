c0ec02d82331b58742ece136218dd12136f55d39
tjakobs <tudilli@web.de>
2014-02-24 10:57:53 +0100

    Grundstruktur fuer ClientStart

--- vs1-ws2013/src/client/ClientStart.java
+++ vs1-ws2013/src/client/ClientStart.java
@@ -8,14 +8,35 @@ package client;
  *
  */
 public class ClientStart {
-
+	
+	private static ClientStart client;
+	private static final int NULL = 0;
+	
+	private int auswahl;
 	/**
 	 * Startpunkt fuer den Client.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		client = new ClientStart();
+		
+		while(auswahl != NULL){
+			client.printMenue();
+			client.auswahlEinlesen();
+			client.ausfuehren();
+		}
 
 	}
-
+	private void printMenue(){
+		// TODO Menueausgabe
+	}
+	
+	private void auswahlEinlesen(){
+		// TODO Einlesen der benutzerauswahl
+	}
+	
+	private String ausfuehren(){
+		// TODO Abschicken der auswahl und Ausgabe des Ergebnisses
+		return null;
+	}
 }
