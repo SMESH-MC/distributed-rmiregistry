4beda24c39e5c6c3a7f9583b3964d43a8b00424c
tjakobs <tudilli@web.de>
2014-02-26 11:30:24 +0100

    Erste Task vorbereitet

---
+++ vs1-ws2013/src/client/ComputeRoot.java
@@ -0,0 +1,38 @@
+/**
+ * Task zur Berechnung der Quadratwurzel
+ */
+package client;
+
+import java.io.Serializable;
+
+import exceptions.ParameterException;
+
+/**
+ * @author Thorsten
+ *
+ */
+public class ComputeRoot implements Task<Integer>, Serializable{
+
+	private static final long serialVersionUID = 1517373870379096195L;
+
+	private int operator;
+	
+	/**
+	 * Konstuktor mit erwartetem Operator
+	 * @param operator
+	 * @throws ParameterException 
+	 */
+	public ComputeRoot(int operator) throws ParameterException {
+		super();
+		if (operator < 0){throw new ParameterException();}
+		this.operator = operator;
+	}
+
+
+	@Override
+	public Integer execute() {
+		// TODO Berechnung der Wurzel einfuegen
+		return null;
+	}
+
+}
---
+++ vs1-ws2013/src/exceptions/ParameterException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class ParameterException extends Exception {
+
+	private static final long serialVersionUID = -4905680713705007912L;
+
+}
