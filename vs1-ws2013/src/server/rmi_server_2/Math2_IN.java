0c45458a97c79ff0ace6521e0e84b257f0d6a40e
arax89 <alexander-kessler@web.de>
2014-03-19 11:31:05 +0100

    Server entfernt

--- vs1-ws2013/src/server/rmi_server_2/Math2_IN.java
+++
@@ -1,8 +0,0 @@
-package rmi_server_2;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Math2_IN extends Remote {
-	int multiplicate(int x, int y) throws RemoteException;
-}
--- vs1-ws2013/src/server/rmi_server_3/HelloWorld_IN.java
+++
@@ -1,8 +0,0 @@
-package rmi_server_3;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface HelloWorld_IN extends Remote {
-	String hello() throws RemoteException;
-}
