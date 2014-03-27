adafa3513526facef60217f0dfefd7214f8a8efb
chschuetz <christopher.schuetz@arcor.de>
2014-03-27 09:02:14 +0100

    Fix für den Server, braucht stub:
    Wollte eigentlich die Compute Klassen in das Projekt Compute verschieben (+exceptions) aber beim build hatte er dann Probleme und ich hab eine ClassNotFoundExceptions bekommen für compute.Compute, was eigentlich ja nicht sein kann. Wäre schön wenn sich das jemand anschaut

---
+++ Server_test/src/ComputeSquare.java
@@ -0,0 +1,49 @@
+
+
+import compute.*;
+import java.rmi.RemoteException;
+
+/**
+ * Classe zum Berechnen des Quadrates
+ * @author Thorsten
+ */
+public class ComputeSquare implements Task {
+
+    private static final long serialVersionUID = 7452870205251216131L;
+
+    private double operand;
+    private int ID;
+
+    /**
+     * Konstruktor mit einem Double Parameter der als Operand hinterlegt wird.
+     * @param operand
+     * @throws RemoteException 
+     */
+    public ComputeSquare(double operand) throws RemoteException {
+        this.operand = operand;
+    }
+
+    /**
+     * Berechnung des Quadrates
+     * @return Quadrat
+     */
+    @Override
+    public Object execute() {
+        return operand * operand;
+    }
+
+    @Override
+    public String taskName() {
+        return "Quadrat aus " + operand + " berechnen...";
+    }
+
+    @Override
+    public int getClientID() {
+        return ID;
+    }
+
+    @Override
+    public void setClientID(int ID) {
+        this.ID = ID;
+    }
+}
